e5c65d7b52f00376b96090b123f4711eb92a02d8
Raj Yadav <rajyadav7047@gmail.com>
2024-01-02 22:08:53 +0530

    finalcommit

--- Mini Projects/08-PASSWORD-GENERATOR/script.js
+++ Mini Projects/08-PASSWORD-GENERATOR/script.js
@@ -29,15 +29,25 @@ handleSlider();
 // set circle color grey
 
 // set [password length]
+
 function handleSlider() {
   inputSlider.value = passwordLength;
   lengthDisplay.innerText = passwordLength;
+
+  const min = inputSlider.min;
+  const max = inputSlider.max;
+  inputSlider.style.backgroundSize =
+    ((passwordLength - min) * 100) / (max - min) + "% 100%";
 }
 
 function setIndicator(color) {
   indicator.style.bagroundColor = color;
   //   shadow
+  indicator.style.boxShadow = `0 0 12px 1px ${color}`;
 }
+
+// Default Indicator
+setIndicator("#ccc");
 
 function getRandomInteger(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -84,6 +94,8 @@ function calcStrength() {
   }
 }
 
+//==================================================================================================================
+
 async function copyContent() {
   try {
     // clipboard pe write kre ga and promise (reslove , return) return krega
@@ -104,7 +116,9 @@ function shufflePassword(array) {
   // fisher yates methods Algorithm
 
   for (let i = array.length - 1; i > 0; i--) {
+    // random J, find out using random function
     const j = Math.floor(Math.random() * (i + 1));
+    // swap number at i index and j index
     const temp = array[i];
     array[i] = array[j];
     array[j] = temp;
