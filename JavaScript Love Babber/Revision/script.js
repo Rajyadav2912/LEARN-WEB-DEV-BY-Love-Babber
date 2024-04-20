1a408fcc5d9be6e908e30a3ee3d9066b28e3a9bb
Raj Yadav <rajyadav7047@gmail.com>
2024-04-20 14:39:09 +0530

    add new

---
+++ JavaScript Love Babber/Revision/script.js
@@ -0,0 +1,29 @@
+//  Fundamental of JavaScript
+// Array and Object
+// async js Coding
+// forEach, amp , filter, find , indexOf
+
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// ForEach function
+// arr.forEach((val) => console.log("Value Hello:", val));
+
+// Map Function
+// arr.map((val) => console.log("value", "15 : ", val));
+
+// Filter Function
+// arr.filter((val) => {
+//   if (val > 3 && val < 8) {
+//     return console.log("greater than 3: ", val);
+//   } else {
+//     return console.log("Not greater than 3:", val);
+//   }
+// });
+
+// // Find Function
+// arr.find((val) => {
+//   if (val > 5 || val > 6) {
+//     return console.log("value", val, ":", val); 
+//   }
+// });
+
