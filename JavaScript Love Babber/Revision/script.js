d1cc851faed75c7ad357a471087c0999e9ae1f66
Raj Yadav <rajyadav7047@gmail.com>
2024-04-21 14:30:06 +0530

    new

--- JavaScript Love Babber/Revision/script.js
+++ JavaScript Love Babber/Revision/script.js
@@ -23,7 +23,53 @@ var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // // Find Function
 // arr.find((val) => {
 //   if (val > 5 || val > 6) {
-//     return console.log("value", val, ":", val); 
+//     return console.log("value", val, ":", val);
 //   }
 // });
 
+// console.log(arr.indexOf(1));
+
+// Object
+// let person = {
+//   name: "Raj Yadav",
+//   age: 21,
+//   phone: 8349020828,
+// };
+
+// How to access 1. Dot notation , 2. Brackets notation
+// 1. Dot notation
+// console.log("Name : ", person.name);
+// console.log("Age : ", person.age);
+// console.log("Phone No : ", person.phone);
+
+// 2. Brackets notation
+
+// console.log("Name : ", person["name"]);
+// console.log("Age : ", person["age"]);
+// console.log("Phone No : ", person["phone"]);
+
+// Arrays:- 1. Adding , 2. Modifying , 3. Deleting
+
+let person = {
+  name: "Raj Yadav",
+  age: 21,
+  phone: 8349020828,
+};
+
+// 1. Adding
+person.isSleeping = false;
+
+console.log(person);
+// { name: 'Raj Yadav', age: 21, phone: 8349020828, isSleeping: false }
+
+// 2. Modifying
+person.age = 22;
+
+console.log(person);
+// { name: 'Raj Yadav', age: 22, phone: 8349020828, isSleeping: false }
+
+// 3. Deleting
+delete person.phone;
+
+console.log(person);
+// { name: 'Raj Yadav', age: 22, isSleeping: false }
---
+++ JavaScript Love Babber/Revision/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+console.log("Name : ", person.name);
+// console.log("Age : ", person.age);
+// console.log("Phone No : ", person.phone);
